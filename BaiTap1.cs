5bbdcac9c2dccda3b57206cb771e1f0f3d4ddf14
LuuNgocTran24T3 <2415053122343@sv.ute.udn.vn>
2026-01-19 13:42:34 +0700

    Create BaiTap1.cs

---
+++ BaiTap1.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Lưu Ngọc Trân");
+        Console.WriteLine("MSSV: 2415053122343");
+    }
+}
